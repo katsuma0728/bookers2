924b6a4171515a962112bc85f442a5736b866479
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-03-26 02:58:12 +0000

    flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,19 +7,37 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(books_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 private
 
-  def books_params
-    params.require(:post_image).permit(:title, :body,)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,43 @@
 class UsersController < ApplicationController
-  
+
   def index
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+    @books = @user.books
   end
-  
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
